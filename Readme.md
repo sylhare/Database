332df99e04f636387c4d809ff85d48fc364b5a20
sylhare <sylhareng@gmail.com>
2017-05-03 10:47:40 -0400

    Zip dhtmlxGantt

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
-# Database
+# Database [![Codacy Badge](https://api.codacy.com/project/badge/Grade/affcc041625a4f1f8a90ef3384fa5135)](https://www.codacy.com/app/Sylhare/Database?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=Sylhare/Database&amp;utm_campaign=Badge_Grade)
 
-While trying the dhtmlxGantt (not made by me) I learnt some basic php and SQL (in the src and sql folders).
+While trying the [dhtmlxGantt](https://dhtmlx.com/docs/products/dhtmlxGantt/), I learnt some basic php and SQL (in the src and sql folders).
 
 For the dhtmlxGantt, I've played with it a little.
 I want to add some explaination on how to set a local webserver to try such as WAMP or else.
--- sql/query_demo.sql
+++ sql/query_demo.sql
@@ -1,13 +1,13 @@
 -- Create the table
-CREATE TABLE `contacts` (
-        `id` int(6) NOT NULL auto_increment,
-        `first` varchar(15) NOT NULL,
-        `last` varchar(15) NOT NULL,
-        `phone` varchar(20) NOT NULL,
-        `mobile` varchar(20) NOT NULL,
-        `fax` varchar(20) NOT NULL,
-        `email` varchar(30) NOT NULL,
-        `web` varchar(30) NOT NULL,
+CREATE TABLE contacts (
+        id int(6) NOT NULL auto_increment,
+        firstname varchar(15) NOT NULL,
+        lastname varchar(15) NOT NULL,
+        phone varchar(20) NOT NULL,
+        mobile varchar(20) NOT NULL,
+        fax varchar(20) NOT NULL,
+        email varchar(30) NOT NULL,
+        web varchar(30) NOT NULL,
         PRIMARY KEY (id),
         UNIQUE id (id),
         KEY id_2 (id)
